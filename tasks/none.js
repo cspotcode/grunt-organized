e38b72f276fa4bfa9f8d5498e78e268ca8342d0b
Andrew Bradley <cspotcode@gmail.com>
2014-12-15 01:59:04 -0500

    Add dummy tasks folder to prevent 'load-grunt-tasks' from logging an error
    - 'load-grunt-tasks' by default tries to `grunt.loadNpmTasks` for every
      dependency named /grunt-*/.
    - By adding a tasks directory with a js file that doesn't actually load
      any grunt tasks, we prevent 'load-grunt-tasks' from logging an error
      saying it can't find our module.

---
+++ tasks/none.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+    /*
+     * Do nothing.
+     * 
+     * This file exists to prevent [load-grunt-tasks](https://www.npmjs.com/package/load-grunt-tasks)
+     * By default, it attempts to load each dependency named "grunt-*"
+     * as a grunt plugin.  It will log an error if it can't
+     * `grunt.loadTasks('grunt-organized/tasks')`
+     * load-grunt-tasks can be configured to skip this module, but that
+     * requires extra work, and it was easier to include this file.
+     */
+};
